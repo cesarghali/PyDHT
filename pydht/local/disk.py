961ef77916dcf11974e1934189ae27dfb67366dd
Cesar Ghali <cesarghali.p@gmail.com>
2016-03-21 11:20:22 -0700

    Add comments to LocalDiskDHT

--- pydht/local/disk.py
+++ pydht/local/disk.py
@@ -3,8 +3,14 @@ import os
 import os.path
 import shutil
 
+
+# LocalDiskDHT implements a DHT stored on disk in a single machine. The DHT
+# contains a list of hash values and their occurrences in the format:
+# <hashValue:occurrences>.
 class LocalDiskDHT:
-    # hashSize is in bits.
+    # hashSize is in bits, numOfHt is the number of hath tables to use in the
+    # distributed hash table, and path is the directory location where the DHT
+    # is stored on disk
     def __init__(self, hashSize, numOfHT, path):
         self.hashSize = hashSize
         self.numOfHT = numOfHT
@@ -23,8 +29,11 @@ class LocalDiskDHT:
             shutil.rmtree(path, ignore_errors=True)
         os.makedirs(path)
 
-    # hashValue is a big (long) integer.
+
+    # insert adds a new hashValue into the DHT. hashValue is an integer or a long
+    # integer.
     def insert(self, hashValue):
+        # Calculate the HT ID of this hashValue.
         htId = self._calculateHTId(hashValue)
 
         # Read the counter of the hash value.
@@ -38,15 +47,18 @@ class LocalDiskDHT:
         # Append the hash with its correct counter. If the hash value is new,
         # counter at this point is 0, otherwise it will have a greater value. In
         # both cases, add 1 to the counter.
-        self.__appendHT(htId, hashValue, counter + 1)
+        self.__append(htId, hashValue, counter + 1)
 
 
+    # _read reads the number of occurrences of a given hashValue.
     def _read(self, hashValue):
+        # Calculate the HT ID of this hash value.
         htId = self._calculateHTId(hashValue)
 
         counter = 0
         fileName = os.path.join(self.path, str(htId))
-        # Open the file and look for the matching hashValue.
+        # Open the corresponding on disk file and look for the matching
+        # hashValue.
         if os.path.isfile(fileName):
             with open(fileName, "r") as htFile:
                 for line in htFile:
@@ -58,22 +70,29 @@ class LocalDiskDHT:
         return counter
 
 
+    # _calculateHTId calculates the HT ID of a given hashValue.
     def _calculateHTId(self, hashValue):
         htId = int(hashValue / self.htSize) + 1
-        # This throws and error if the htId is larger than number of hash tables.
+        # This throws and error if htId is larger than number of hash tables.
         assert htId <= self.numOfHT
         return htId
 
 
-    # Append hashValue to the end of htId hash table.
-    def __appendHT(self, htId, hashValue, counter):
+    # _append appends hashValue to the end of htId hash table.
+    def __append(self, htId, hashValue, counter):
         fileName = os.path.join(self.path, str(htId))
         with open(fileName, "a+") as htFile:
             htFile.write(str(hashValue) + ":" + str(counter) + "\n")
 
 
-    # Copy the specified htID hash table without the corresponding hashValue.
+    # __copyHT copies the specified htID hash table without a given hashValue.
     def __copyHT(self, htId, hashValue):
+        # Steps:
+        #   1. Rename the current htId file with '.tmp' extension.
+        #   2. Create an empty file for htId.
+        #   3. Copy all hash values from the '.tmp' file to the empty file,
+        #      without the given hashValue.
+        #   4. Remove the '.tmp' file.
         fileName = os.path.join(self.path, str(htId))
         tmpFileName = os.path.join(self.path, str(htId) + ".tmp")
         os.rename(fileName, tmpFileName)
@@ -91,9 +110,12 @@ class LocalDiskDHT:
         os.remove(tmpFileName)
 
 
-    # Calculate the collision probability of this hash table.
+    # calculateCollision calculates the collision probability of this hash table.
     def calculateCollision(self):
+        # Total contains the sum of all hash value occurrences in the DHT.
         total = 0
+        # Count contains the sum of occurrences of hash values with collision,
+        # e.g., occurrence value larger than 1.
         count = 0
         for filePath in [os.path.join(self.path, f) for f in
                          os.listdir(self.path) if
