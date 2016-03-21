705e0e562a2106f612df8551e8aca8f7e162bd3f
Cesar Ghali <cesarghali.p@gmail.com>
2016-03-20 21:20:12 -0700

    Minor fix

--- pydht/__init__.py
+++ pydht/__init__.py
@@ -1,5 +1,6 @@
 """
-Pydht is a python implementation of a distributed hash table.
+Pydht is a Python implementation of distributed hash tables stored on a local
+machine.
 """
 version_info = (0, 1)
 version = '.'.join(map(str, version_info))
